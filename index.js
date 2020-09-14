dec1d40c7b4cd728c4f4a88961a7f142fe936308
Mukhammadsher <mr.muhammadsher@mail.ru>
2020-09-14 16:20:31 +0500

    Default rout

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.use(express.json());
 
 app.get("/rank", rank);
 
+app.use((req, res) => {
+  res.send({ status: 404, message: "Not found" });
+});
+
 app.listen(process.env.PORT || 3000, "0.0.0.0", () =>
   console.log(`Server started on port: ${process.env.PORT || 3000}`)
 );
