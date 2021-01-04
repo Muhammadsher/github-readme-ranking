4487ad9c4128d4485970f3037d7c1116e6e761f6
Mukhammadsher <mr.muhammadsher@mail.ru>
2021-01-04 08:40:37 +0500

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express();
-const rank = require('./api/rank')
-
-app.get('/', (req, res) => {
-    rank(req, res)
-})
-
-app.listen(3000, () => console.log(`Server started on port 3000`))
